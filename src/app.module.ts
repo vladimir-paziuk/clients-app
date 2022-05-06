d7f6c857cebdd1923f20502f8c442b0da87b07fb
vladimir.paziuk <vladimir.paziuk@itrexgroup.com>
2022-05-06 10:34:16 +0300

    Install and use typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { DoctorsModule } from './doctors/doctors.module';
 
 @Module({
-  imports: [DoctorsModule],
+  imports: [
+    DoctorsModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'clients-management',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
