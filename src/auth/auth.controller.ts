ae9b121c635bfe5637a430ff7a0251bedfbe457e
vladimir.paziuk <vladimir.paziuk@itrexgroup.com>
2022-05-11 12:53:22 +0300

    Swagger API Error response

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,19 +2,26 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { JwtToken } from 'common/jwt/jwt.strategy';
 import { AuthCredentialsDto } from './dtos/authCredentialsDto';
-import { ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import { ApiConflictResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import { swaggerApiErrorResponse } from 'common/swagger/swagger-api-error-response';
 
 @ApiTags('Auth')
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
+  @ApiOkResponse({ description: 'User created' })
+  @ApiConflictResponse({
+    description: 'User name already exist',
+  })
+  @swaggerApiErrorResponse()
   @Post('/sign-up')
   signUp(@Body() credentials: AuthCredentialsDto): Promise<void> {
     return this.authService.signUp(credentials);
   }
 
-  @ApiOkResponse({ type: JwtToken })
+  @ApiOkResponse({ type: JwtToken, description: 'User logged in' })
+  @swaggerApiErrorResponse()
   @Post('/sign-in')
   signIn(@Body() authCredentialsDto: AuthCredentialsDto): Promise<JwtToken> {
     return this.authService.signIn(authCredentialsDto);
--- src/doctors/doctors.controller.ts
+++ src/doctors/doctors.controller.ts
@@ -15,6 +15,7 @@ import { DoctorEntity } from './doctor.entity';
 import { DoctorDto } from './dtos/doctorDto';
 import { ApiBearerAuth, ApiOkResponse, ApiTags } from '@nestjs/swagger';
 import { AUTH_BEARER_DEFAULT } from 'common/swagger/swagger.config';
+import { swaggerApiErrorResponse } from 'common/swagger/swagger-api-error-response';
 import { DoctorQueryDto } from './dtos/doctorQueryDto';
 
 @ApiBearerAuth(AUTH_BEARER_DEFAULT)
@@ -24,25 +25,41 @@ import { DoctorQueryDto } from './dtos/doctorQueryDto';
 export class DoctorsController {
   constructor(private doctorsService: DoctorsService) {}
 
-  @ApiOkResponse({ type: [DoctorEntity] })
+  @ApiOkResponse({
+    type: [DoctorEntity],
+    description: 'Get all doctors',
+  })
+  @swaggerApiErrorResponse()
   @Get()
   getDoctors(@Query() query: DoctorQueryDto): Promise<DoctorEntity[]> {
     return this.doctorsService.getData(query);
   }
 
-  @ApiOkResponse({ type: DoctorEntity })
+  @ApiOkResponse({
+    type: DoctorEntity,
+    description: 'Get selected doctor',
+  })
+  @swaggerApiErrorResponse()
   @Get('/:id')
   getDoctorById(@Param('id') id: string): Promise<DoctorEntity> {
     return this.doctorsService.getEntityById(id);
   }
 
-  @ApiOkResponse({ type: DoctorEntity })
+  @ApiOkResponse({
+    type: DoctorEntity,
+    description: 'Create doctor',
+  })
+  @swaggerApiErrorResponse()
   @Post()
   createDoctor(@Body() body: DoctorDto): Promise<DoctorEntity> {
     return this.doctorsService.createEntity(body);
   }
 
-  @ApiOkResponse({ type: DoctorEntity })
+  @ApiOkResponse({
+    type: DoctorEntity,
+    description: 'Update doctor',
+  })
+  @swaggerApiErrorResponse()
   @Patch('/:id')
   updateDoctorById(
     @Param('id') id: string,
@@ -51,6 +68,10 @@ export class DoctorsController {
     return this.doctorsService.updateDoctorById(id, desc);
   }
 
+  @ApiOkResponse({
+    description: 'Delete doctor',
+  })
+  @swaggerApiErrorResponse()
   @Delete('/:id')
   deleteDoctorById(@Param('id') id: string): Promise<void> {
     return this.doctorsService.deleteEntityById(id);
