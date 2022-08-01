c50bea21debd122f0536a76a963b1ee73b422d6c
vladimir.paziuk <vladimir.paziuk@itrexgroup.com>
2022-08-01 13:23:25 +0300

    Sorting indexes

--- apps/clinic/migrations/1658405108139-CreateDoctorsIndex.ts
+++ apps/clinic/migrations/1658405108139-CreateDoctorsIndex.ts
@@ -4,11 +4,19 @@ export class CreateDoctorsIndex1658405108139 implements MigrationInterface {
   name = 'CreateDoctorsIndex1658405108139';
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
-      CREATE INDEX doctors_specialization ON clinic.doctors(specialization);
+      CREATE INDEX doctors_specialization_idx ON clinic.doctors(specialization);
+    `);
+    await queryRunner.query(`
+      CREATE INDEX doctors_created_at_idx ON clinic.doctors(created_at);
+    `);
+    await queryRunner.query(`
+      CREATE INDEX doctors_updated_at_idx ON clinic.doctors(updated_at);
     `);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`DROP INDEX doctors_specialization_idx;`);
+    await queryRunner.query(`DROP INDEX doctors_created_at_idx;`);
+    await queryRunner.query(`DROP INDEX doctors_updated_at_idx;`);
   }
 }
